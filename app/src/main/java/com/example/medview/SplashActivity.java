783324e0d9560ff9f81b0676d23a5d2d51e8af96
Aarika <aarika.srivastava@gmail.com>
2022-03-14 13:36:31 +0530

    splash animation

--- app/src/main/java/com/example/medview/SplashActivity.java
+++ app/src/main/java/com/example/medview/SplashActivity.java
@@ -8,15 +8,41 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    //variables for anim
+    Animation topAnim , bottomAnim;
+    //splash screen elemnts
+    ImageView imageViewBig , imageViewSmall;
+    TextView textView ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_splash);
 
         getSupportActionBar().hide();
+
+        //load animation
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_splash_anim);
+        bottomAnim =AnimationUtils.loadAnimation(this,R.anim.bottom_splash_anim);
+
+        imageViewBig =findViewById(R.id.imageView);
+        imageViewSmall=findViewById(R.id.imageView2);
+        textView=findViewById(R.id.textView);
+
+        imageViewBig.setAnimation(topAnim);
+        imageViewSmall.setAnimation(bottomAnim);
+       textView.setAnimation(bottomAnim);
+
+
 
         Handler handler = new Handler();
 
@@ -40,6 +66,6 @@ public class SplashActivity extends AppCompatActivity {
                     finish();
                 }
             }
-        }, 2000);
+        }, 5000);
     }
 }
