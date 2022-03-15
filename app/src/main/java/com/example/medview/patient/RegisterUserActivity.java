a6f62e931347452e306b68740f236b5e23852f91
Aarika <aarika.srivastava@gmail.com>
2022-03-15 16:20:33 +0530

    added doctors's layout 2

---
+++ app/src/main/java/com/example/medview/patient/RegisterUserActivity.java
@@ -0,0 +1,16 @@
+package com.example.medview.patient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.medview.R;
+
+public class RegisterUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_user);
+    }
+}
