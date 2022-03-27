9ab4a2785c0c26445667151c203ff0367ac003f8
Suman5501 <sumankri5501@gmail.com>
2022-03-27 23:02:20 +0530

    progressbar added

--- app/src/main/java/com/example/medview/patient/IllnessActivity.java
+++ app/src/main/java/com/example/medview/patient/IllnessActivity.java
@@ -5,12 +5,15 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 
 import com.example.medview.R;
 import com.example.medview.adapters.IllnessDataAdapter;
 import com.example.medview.adapters.RecordDataAdapter;
 import com.example.medview.models.Illness;
 import com.example.medview.models.PatientHealthRecord;
+import com.facebook.shimmer.ShimmerFrameLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,11 +24,26 @@ public class IllnessActivity extends AppCompatActivity {
     RecyclerView illnessRecyclerView;
     IllnessDataAdapter illnessDataAdapter;
 
+    ShimmerFrameLayout shimmerFrameLayout;
     List<Illness> illnessList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_illness);
+
+        shimmerFrameLayout = findViewById(R.id.shimmerLayout);
+
+
+        shimmerFrameLayout.startShimmer();
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                shimmerFrameLayout.setVisibility(View.GONE);
+                illnessRecyclerView.setVisibility(View.VISIBLE);
+            }
+        }, 2000);
 
 
         illnessRecyclerView = findViewById(R.id.illness_recycler_view);
